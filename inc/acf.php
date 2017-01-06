0b2e6890f44427d87c49bb31de1aea4af7e0fcb7
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-06 01:14:33 +0100

    Added footer fields

--- inc/acf.php
+++ inc/acf.php
@@ -10,3 +10,100 @@ if (function_exists('acf_add_options_page')) {
   
 }
 
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_586ede293d674',
+	'title' => 'Opzioni',
+	'fields' => array (
+		array (
+			'placement' => 'top',
+			'endpoint' => 0,
+			'key' => 'field_586ede3520943',
+			'label' => 'Footer',
+			'name' => '',
+			'type' => 'tab',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+		),
+		array (
+			'default_value' => '',
+			'maxlength' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'key' => 'field_586ede4720944',
+			'label' => 'Copyright',
+			'name' => 'options_footer_copyright',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+		),
+		array (
+			'default_value' => '',
+			'maxlength' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'key' => 'field_586edef612daa',
+			'label' => 'Privacy Testo',
+			'name' => 'options_footer_privacy_text',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '50',
+				'class' => '',
+				'id' => '',
+			),
+		),
+		array (
+			'default_value' => '',
+			'placeholder' => '',
+			'key' => 'field_586edec012da9',
+			'label' => 'Privacy link',
+			'name' => 'options_footer_privacy_url',
+			'type' => 'url',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '50',
+				'class' => '',
+				'id' => '',
+			),
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'options_page',
+				'operator' => '==',
+				'value' => 'options-theme',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
