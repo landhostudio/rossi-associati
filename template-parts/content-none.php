5806f8c55e4668c6919211666482519f98729844
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-10 20:39:17 +0100

    Added "none" part

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,4 +1,8 @@
-<article class="none">
-  <h2><?php esc_html_e('Contenuto non esiste', 'rossi-associati'); ?></h2>
-  <p><?php esc_html_e('Non ci sono i contenuti da visualizzare.', 'rossi-associati'); ?></p>
+<article class="page page--none">
+  <div class="page__content">
+    <div class="container">
+      <h2 class="page__title"><?php esc_html_e('Contenuto non esiste', 'rossi-associati'); ?></h2>
+      <p><?php esc_html_e('Non ci sono i contenuti da visualizzare.', 'rossi-associati'); ?></p>
+    </div>
+  </div>
 </article>
