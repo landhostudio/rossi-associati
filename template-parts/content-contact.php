49263dab26fce323b85b0fe176580b5f064bb4e1
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-10 20:39:29 +0100

    Added contact part

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -1,28 +1,39 @@
-<article class="contact">
-  <?php if (get_field('page_subtitle')): ?>
-    <h2><?php the_field('page_subtitle'); ?></h2>
-  <?php else: ?>
-    <h2><?php the_title(); ?></h2>
-  <?php endif; ?>
-  <?php the_post_thumbnail(); ?>
-  <?php the_content(); ?>
-
-  <?php if (have_rows('contact_locations')): ?>
-    <div class="contact__locations">
-    <?php while (have_rows('contact_locations')): the_row(); ?>
-      <div class="contact__location">
-
-        <?php if (get_sub_field('contact_locations_title')): ?>
-          <h3><?php the_sub_field('contact_locations_title'); ?></h3>
-        <?php endif; ?>
-
-        <?php if (get_sub_field('contact_locations_text')): ?>
-          <?php the_sub_field('contact_locations_text'); ?>
-        <?php endif; ?>
-
+<article class="page page--contact">
+  <div class="page__cover">
+    <div class="page__container">
+      <div class="page__image">
+        <?php the_post_thumbnail(); ?>
       </div>
-    <?php endwhile; ?>
     </div>
-  <?php endif; ?>
-  
+  </div>
+
+  <div class="page__content">
+    <div class="container">
+      <?php if (get_field('page_subtitle')): ?>
+        <h2 class="page__title"><?php the_field('page_subtitle'); ?></h2>
+      <?php else: ?>
+        <h2 class="page__title"><?php the_title(); ?></h2>
+      <?php endif; ?>
+
+      <?php the_content(); ?>
+    </div>
+
+    <?php if (have_rows('contact_locations')): ?>
+      <div class="contact__locations">
+      <?php while (have_rows('contact_locations')): the_row(); ?>
+        <div class="contact__location">
+
+          <?php if (get_sub_field('contact_locations_title')): ?>
+            <h3><?php the_sub_field('contact_locations_title'); ?></h3>
+          <?php endif; ?>
+
+          <?php if (get_sub_field('contact_locations_text')): ?>
+            <?php the_sub_field('contact_locations_text'); ?>
+          <?php endif; ?>
+
+        </div>
+      <?php endwhile; ?>
+      </div>
+    <?php endif; ?>
+  </div>
 </article>
