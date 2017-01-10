55bd3e920ff9f5eb97ccb7f66937da1855c38ca9
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-10 22:01:06 +0100

    Added part footer

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
-    <footer role="contentinfo">
-      <p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php if (get_field('options_footer_copyright', option)): ?> <?php the_field('options_footer_copyright', option); ?><?php endif; ?><?php if (get_field('options_footer_privacy_text', option) && get_field('options_footer_privacy_url', option)): ?> <a href="<?php the_field('options_footer_privacy_url', option); ?>" target="_blank" rel="nofollow"><?php the_field('options_footer_privacy_text', option); ?></a><?php endif; ?></p>
+    <footer role="contentinfo" class="footer">
+      <div class="container">
+        <p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php if (get_field('options_footer_copyright', option)): ?> <?php the_field('options_footer_copyright', option); ?><?php endif; ?><?php if (get_field('options_footer_privacy_text', option) && get_field('options_footer_privacy_url', option)): ?> <a href="<?php the_field('options_footer_privacy_url', option); ?>" target="_blank" rel="nofollow"><?php the_field('options_footer_privacy_text', option); ?></a><?php endif; ?></p>
+      </div>
     </footer>
     <?php wp_footer(); ?>
   </body>
