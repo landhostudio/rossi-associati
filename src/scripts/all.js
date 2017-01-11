9fc9ea7beb4e2a2e5edd22ac94bacc7bda0ac932
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-11 22:09:28 +0100

    Removed page cover container padding on small device only

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -7,12 +7,32 @@ $(function () {
   
   var init = function () {
     initHeader();
+    initList();
   };
 
   function initHeader() {
     $('.toggle').click(function(event) {
       $('.header').toggleClass('header--opened');
     });
+  };
+
+  function initList() {
+
+    $('.list__element a').click(function(event) {
+
+      var list_id = ($(this).attr('href'));
+
+      $('.list__element').removeClass('list__element--active');
+      $(this).parent().addClass('list__element--active');
+
+      $('.list__item').removeClass('list__item--active');
+      $('.list__item' + list_id).addClass('list__item--active');
+
+    });
+
+    var hash = $.trim(window.location.hash);
+    if (hash) $('.list__element a[href$="'+hash+'"]').trigger('click');
+
   };
 
   init();
