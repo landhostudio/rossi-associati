04b27a2d5f43d63e3ccd331379b95a8efd008cbf
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-12 00:04:58 +0100

    Added site description to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer role="contentinfo" class="footer">
       <div class="container">
-        <p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php if (get_field('options_footer_copyright', option)): ?> <?php the_field('options_footer_copyright', option); ?><?php endif; ?><?php if (get_field('options_footer_privacy_text', option) && get_field('options_footer_privacy_url', option)): ?> <a href="<?php the_field('options_footer_privacy_url', option); ?>" target="_blank" rel="nofollow"><?php the_field('options_footer_privacy_text', option); ?></a><?php endif; ?></p>
+        <p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?> – <?php bloginfo('description'); ?>. <?php if (get_field('options_footer_copyright', option)): ?> <?php the_field('options_footer_copyright', option); ?><?php endif; ?><?php if (get_field('options_footer_privacy_text', option) && get_field('options_footer_privacy_url', option)): ?> <a href="<?php the_field('options_footer_privacy_url', option); ?>" target="_blank" rel="nofollow"><?php the_field('options_footer_privacy_text', option); ?></a><?php endif; ?></p>
       </div>
     </footer>
     <?php wp_footer(); ?>
