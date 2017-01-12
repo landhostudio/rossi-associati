44c9eab1f3850882831b332c96cf248f17554561
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-12 01:40:04 +0100

    Added part single

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,5 +1,23 @@
-<article class="single">
-  <h2><?php the_title(); ?></h2>
-  <?php the_post_thumbnail(); ?>
-  <?php the_content(); ?>
+<article class="page page--single">
+  <div class="page__cover">
+    <div class="page__container">
+      <div class="page__image">
+        <?php the_post_thumbnail(); ?>
+      </div>
+    </div>
+  </div>
+
+  <div class="page__body">
+    <div class="container">
+      <?php if (get_field('page_subtitle')): ?>
+        <h2 class="page__title"><?php the_field('page_subtitle'); ?></h2>
+      <?php else: ?>
+        <h2 class="page__title"><?php the_title(); ?></h2>
+      <?php endif; ?>
+
+      <div class="page__content">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>  
 </article>
