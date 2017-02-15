6adf1e341b9876dad6e5487a3e5e1123465c3228
h0r0man <roman.horokhovatskyy@gmail.com>
2017-02-15 15:41:05 +0100

    Removed alternative style in contact page

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -1,4 +1,4 @@
-<article class="page page--contact page--alternative">
+<article class="page page--contact">
   <?php if (has_post_thumbnail()): ?>
     <div class="page__cover">
       <div class="page__container">
